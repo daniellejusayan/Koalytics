f12ebdf0e279577983ea081f61e529e86c40da9e
daniellejusayan <6731503051@lamduan.mfu.ac.th>
2025-10-16 15:26:10 +0700

    suggested core tables in database/schema

---
+++ database/schema.sql
@@ -0,0 +1,17 @@
+
+
+--all id will be PK pls thank you!
+
+-- Core tables:
+-- Users
+-- Projects
+-- Project Roles
+-- Collaborations
+-- Test Session
+-- SUS Questionnaires
+-- Facial Data
+-- Responses
+-- Test Results
+-- Emotion Analysis
+-- Task Metrics
+-- Rewards
